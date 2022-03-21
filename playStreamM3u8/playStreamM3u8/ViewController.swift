a4a295ece2a9e398f3b8734317768ec1e0e6c2bb
phungvantung <phungtung684@gmail.com>
2022-03-21 23:37:39 +0700

    init project

---
+++ playStreamM3u8/playStreamM3u8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playStreamM3u8
+//
+//  Created by Phùng Tùng on 21/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
